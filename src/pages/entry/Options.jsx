54b849acf86f9d6e706d5c154a6652308d14914f
kei178 <keisuke.inaba.cpa@gmail.com>
2021-05-18 20:42:48 -0500

    Add Scoop Option features and testing

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ScoopOption from './ScoopOptions';
+import { Row } from 'react-bootstrap';
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((err) => {
+        // TODO: handle error message
+        console.log(err);
+      });
+  }, [optionType]);
+
+  // TODO: replace null with ToppingOptions
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => {
+    const { name, imagePath } = item;
+    return <ItemComponent key={name} name={name} imagePath={imagePath} />;
+  });
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
