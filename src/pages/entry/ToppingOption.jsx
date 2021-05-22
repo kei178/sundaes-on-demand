f75de62d9b15ddec487ee6a3aa4b0aba6ddbd534
kei178 <keisuke.inaba.cpa@gmail.com>
2021-05-21 21:32:29 -0500

    Add subtotal to ToppingOption

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,6 @@
-import { Col } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +8,15 @@ const ToppingOption = ({ name, imagePath }) => {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-count`}>
+        <Form.Check
+          type="checkbox"
+          onChange={(e) => {
+            updateItemCount(name, e.target.checked ? 1 : 0);
+          }}
+          label={name}
+        />
+      </Form.Group>
     </Col>
   );
 };
