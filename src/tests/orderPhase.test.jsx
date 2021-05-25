28aa2c0c09c9c4c2fff757910c6886f206271b06
kei178 <keisuke.inaba.cpa@gmail.com>
2021-05-25 16:56:34 -0500

    Add test to assert toppings header not in the doc in order summary

--- src/tests/orderPhase.test.jsx
+++ src/tests/orderPhase.test.jsx
@@ -104,3 +104,35 @@ test('order phases for happy path', async () => {
   await screen.findByRole('spinbutton', { name: 'Vanilla' });
   await screen.findByRole('checkbox', { name: 'Cherries' });
 });
+
+test('Toppings header is not on summary page if no toppings ordered', async () => {
+  render(<App />);
+
+  // add ice cream scoops and toppings
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+
+  const chocolateInput = screen.getByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, '2');
+
+  const orderSummaryButton = screen.getByRole('button', {
+    name: /order sundae/i,
+  });
+  userEvent.click(orderSummaryButton);
+
+  const scoopsHeading = screen.getByRole('heading', {
+    name: 'Scoops: $6.00',
+  });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.queryByRole('heading', {
+    name: /toppings/i,
+  });
+  expect(toppingsHeading).not.toBeInTheDocument();
+});
